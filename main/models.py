3529aeb32359030aea0ac41ae4d46deb62a7cabf
sanyashetty <shettysanya@gmail.com>
2022-11-28 12:56:23 -0500

    adding files

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+    body = models.TextField(max_length=300)
+    #ForeignKey is for many-to-one relationship --> many tweets to one user
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    created_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.body
--- main/views.py
+++ main/views.py
@@ -1,4 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Tweet
+from datetime import datetime
 
 def main_view(request):
-    return render(request, 'main.html' )
+    if not request.user.is_authenticated:
+        return redirect('/splash/')
+    if request.method == 'POST' and request.POST['body'] != "":
+        tweet = Tweet.objects.create(
+            body=request.POST['body'],
+            author=request.user,
+            created_at=datetime.now()
+        )
+        tweet.save()
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'main.html', {'tweets': tweets})
+
+
+def splash_view(request):
+    return render(request, 'splash.html')
+
+
+def login_view(request):
+    # getting user info from form to log in an existing user
+    username, password = request.POST['username'], request.POST['password']
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+    else:
+        # if invalid user, give error message
+        return redirect('/splash?error=LoginError')
+
+
+def signup_view(request):
+    # checking is password is at least 8 characters
+    if len(request.POST['password']) < 8:
+        return redirect('/splash?error=SignupError')
+
+    # getting user info from form to sign up a new user
+    user = User.objects.create_user(
+        username=request.POST['username'],
+        password=request.POST['password'],
+        email=request.POST['email'],
+    )
+    login(request, user)
+    return redirect('/')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/splash')
+
+def delete_view(request):
+    tweet = Tweet.objects.get(id=request.GET['id'])
+    if tweet.author == request.user:
+        tweet.delete()
+    return redirect('/')
+
+def like_tweet(request):
+    tweet = Tweet.objects.get(id=request.GET['id'])
+    if len(tweet.likes.filter(username=request.user.username)) == 0:
+        tweet.likes.add(request.user)
+    else:
+        tweet.likes.remove(request.user)
+    tweet.save()
+    return redirect('/')
